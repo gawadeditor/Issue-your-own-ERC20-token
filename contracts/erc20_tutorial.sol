d54e911921552e8f2d9b0dd282a5c8256a3d44bc
gawadeditor <muhamed.fcp@gmail.com>
2024-03-03 19:29:25 +0400

    Ghostcoin.sol

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.24;
 // ----------------------------------------------------------------------------
 // '0Fucks' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
-// Decimals    : 18
+// Deployed to : 0xaB6C409cdE4dccA592C51746DbBD69C4fdEE96Da
+// Symbol      : GHC
+// Name        : Ghostcoin
+// Total supply: 50000000000
+// Decimals    : 9
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract Ghostcoin is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        symbol = "GHC";
+        name = "Ghostcoin";
+        decimals = 9;
+        _totalSupply = 50000000000;
+        balances[0xaB6C409cdE4dccA592C51746DbBD69C4fdEE96Da] = _totalSupply;
+        emit Transfer(address(0), 0xaB6C409cdE4dccA592C51746DbBD69C4fdEE96Da, _totalSupply);
     }
 
 
